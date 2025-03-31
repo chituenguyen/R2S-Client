1014169cba37733a03b76858113863c723b172b0
anhquan268 <anhquan2682001@gmail.com>
2025-03-31 19:35:22 +0700

    [Quan] commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="w-full bg-white">
       <Header />
-      <main className="flex-grow max-w-7xl mx-auto pt-12 md:pt-4 lg:pt-4">{children}</main>
+      <main className="flex-grow max-w-7xl mx-auto pt-12 md:pt-4 lg:pt-4 pl-4 pr-4">{children}</main>
       <Footer />
     </div>
   );
